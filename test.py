d864d567b5a53e8cd2e8f726bb2162f6bff91123
Manisha <136002600+manishapinjari@users.noreply.github.com>
2024-10-25 14:50:58 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+__init__.py
+import pytest # type: ignore
+from app import app
+
+
+@pytest.fixture # type: ignore
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_app_is_working(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello World!" in response.data
